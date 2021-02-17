42b764f494310fc411f237f6e3ebddacf9906aec
Alev-Narcin <76040790+Alev-Narcin@users.noreply.github.com>
2021-02-17 17:21:20 +0300

    regex oluşturuldu.

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -14,10 +17,16 @@ public class User {
     @GeneratedValue
     private Long id;
 
+    @NotNull
+    @Size(min = 4, max = 255)
     private String username;
 
+    @NotNull
+    @Size(min = 4, max = 255)
     private String displayName;
 
+    @NotNull
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}")
     private String password;
 
     @Override
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,35 +1,42 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 public class UserController {
 
-    //private static final Logger log = LoggerFactory.getLogger(UserController.class);
-
     @Autowired
     UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public ResponseEntity<?> createUser(@RequestBody User user) {
-        //log.info(user.toString());
-        String username = user.getUsername();
-
-        if(username == null || username.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public GenericResponse createUser(@Valid @RequestBody User user) {
 
         userService.save(user);
-        return ResponseEntity.ok(new GenericResponse("user created")); //@AllArgsConstructor eklersek buraya parametre eklemek gerekir.
+        return new GenericResponse("user created");         //@AllArgsConstructor eklersek buraya parametre eklemek gerekir.
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+        ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
+        Map<String, String> validationErrors = new HashMap<>();
+
+        for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        error.setValidationError(validationErrors);
+        return error;
     }
 }

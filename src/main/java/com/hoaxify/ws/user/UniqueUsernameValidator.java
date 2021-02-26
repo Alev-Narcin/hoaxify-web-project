edbec302799f13feacbf0aa9da2c2d62d81c0c90
Alev-Narcin <76040790+Alev-Narcin@users.noreply.github.com>
2021-02-26 16:04:27 +0300

    isValid düzenlendi

--- src/main/java/com/hoaxify/ws/user/UniqueUsernameValidator.java
+++ src/main/java/com/hoaxify/ws/user/UniqueUsernameValidator.java
@@ -13,9 +13,6 @@ public class UniqueUsernameValidator implements ConstraintValidator<UniqueUserna
     @Override
     public boolean isValid(String username, ConstraintValidatorContext context) {  //true yada false olduğuna karar vermek için kullanılıyor
         User user = userRepository.findByUsername(username);
-        if (user != null) {
-            return false;
-        }
-        return true;
+        return user == null;
     }
 }

f64cf6b14046b9577ff9eb6f355e7d0b27307a8c
Alev-Narcin <76040790+Alev-Narcin@users.noreply.github.com>
2021-03-22 09:47:47 +0300

    image file lar client tarafında cache lendi

---
+++ src/main/java/com/hoaxify/ws/configuration/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.hoaxify.ws.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        //http://localhost:8080/images/profile.png
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:./picture-storage/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));   //set edilen image ı 1 yıl boyunca client tarafında cache liyor.
+    }
+}
